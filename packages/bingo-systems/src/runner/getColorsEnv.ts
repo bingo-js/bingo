d6aca907773197f71c6a8bb7d28040d782426057
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2026-08-21 15:40:08 -0400

    Apply suggestion from @JoshuaKGoldberg

--- packages/bingo-systems/src/runner/getColorsEnv.ts
+++ packages/bingo-systems/src/runner/getColorsEnv.ts
@@ -3,10 +3,6 @@ interface ColorsStream {
 	isTTY: boolean;
 }
 
-/**
- * Commands run through execa don't see a TTY, so tools such as ESLint disable
- * their colors unless an env variable tells them the colors can be rendered.
- */
 export function getColorsEnv(stdout: ColorsStream = process.stdout) {
 	if (!stdout.isTTY) {
 		return undefined;
