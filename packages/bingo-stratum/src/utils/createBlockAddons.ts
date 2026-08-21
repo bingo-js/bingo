cfc94da2660066f14b95d76caf6c99fd1432b17a
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2026-08-21 14:20:47 -0400

    Apply suggestion from @JoshuaKGoldberg

--- packages/bingo-stratum/src/utils/createBlockAddons.ts
+++ packages/bingo-stratum/src/utils/createBlockAddons.ts
@@ -5,9 +5,6 @@ export function createBlockAddons<Addons extends object, Block>(
 ) {
 	const created = { addons, block };
 
-	// Test framework snapshots print Blocks as an unhelpful [Function], so this
-	// gives them a serialization that says which Block the Addons are for.
-	// https://github.com/bingo-js/bingo/issues/314
 	if (name) {
 		Object.defineProperty(created, "toJSON", {
 			value: () => ({ addons, block: `[Block ${name}]` }),
