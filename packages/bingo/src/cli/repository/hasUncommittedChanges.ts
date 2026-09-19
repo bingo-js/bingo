b3d44628e307968d2c05f04d067fa74fe3aad9eb
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2026-09-19 17:13:39 -0400

    Apply suggestion from @JoshuaKGoldberg

--- packages/bingo/src/cli/repository/hasUncommittedChanges.ts
+++ packages/bingo/src/cli/repository/hasUncommittedChanges.ts
@@ -5,6 +5,5 @@ export async function hasUncommittedChanges(runner: SystemRunner) {
 		"git status --porcelain --untracked-files=normal",
 	);
 
-	// If the status can't be read, the tree can't be verified as safe to clear.
 	return status.failed || !!status.stdout?.toString().trim();
 }
